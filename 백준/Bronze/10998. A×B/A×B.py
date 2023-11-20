5ba77312e4a8b3c9ce9c8edf131c665f8c3b5368
ugialol <150674025+ugialol@users.noreply.github.com>
2023-11-20 20:38:40 +0900

    [Bronze V] Title: A×B, Time: 44 ms, Memory: 31252 KB -BaekjoonHub

---
+++ 백준/Bronze/10998. A×B/A×B.py
@@ -0,0 +1,6 @@
+A,B=map(int,input().split(' '))
+# map함수는 자료형을 바꿔주는것
+# input().split(' ')        :입력한값을 공백을 기준으로 나누는것
+# int                       :입력한값을 정수로 변환하는것
+# map(int,input().split(' ')):입력한 값을 공배을 기준으로 나누어서 정수로 변환
+print(A*B)
